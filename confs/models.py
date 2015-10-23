319528a3dc2a4b6d56fb1d2dccb3211d27de94c7
Guillaume Doré <dore_g@epita.fr>
2015-10-23 15:43:44 +0200

    add confs, authors, TP, videos

--- confs/models.py
+++ confs/models.py
@@ -3,14 +3,27 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Speaker(models.Model):
+    speaker_name = models.CharField('Nom', max_length=200)
+    speaker_firstname = models.CharField('Prénom', max_length=200, default='')
+    speaker_pseudo = models.CharField('Pseudo', max_length=200,
+            default=' ',)
+    speaker_website = models.URLField('Website', max_length=200, default='')
+    
+    def __str__(self):
+        return self.speaker_firstname + (' ' if self.speaker_pseudo == ' '
+                else (' \"' + self.speaker_pseudo + '\" ')) + self.speaker_name
+
+
 class Conf(models.Model):
     conf_title = models.CharField('Titre', max_length=200)
-    conf_description = models.TextField('Description', max_length=1000)
+    conf_description = models.TextField('Description', max_length=2048)
     conf_date = models.DateTimeField('Date', default=timezone.now)
     conf_location = models.CharField('Lieu', max_length=200)
-
+    conf_speakers = models.ManyToManyField(Speaker)
     def __str__(self):
         return self.conf_title
+
 
 class TPInfo(models.Model):
     conf = models.ForeignKey(Conf)
@@ -18,13 +31,13 @@ class TPInfo(models.Model):
     tpinfo_title = models.CharField('Titre', max_length=200)
 
     def __str__(self):
-        return self.tpinfos_title
+        return self.tpinfo_title
 
 
 class VideoInfo(models.Model):
     conf = models.ForeignKey(Conf)
-    videoinfo_link = models.URLField('URL')
+    videoinfo_url = models.URLField('URL')
     videoinfo_title = models.CharField('Titre', max_length=200)
 
     def __str__(self):
-        return self.videoinfo_link
+        return self.videoinfo_url
--- confs/views.py
+++ confs/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from .models import Conf
+
 # Create your views here.
 
 def index(request):
-    template = loader.get_template()
+    template = loader.get_template('confs/index.html')
+    conf_list = Conf.objects.all()
+    context = RequestContext(request, {'conf_list': conf_list})
+    return HttpResponse(template.render(context))
 
-def details(resquest, conf_id):
-    pass
+def details(request, conf_id):
+    template = loader.get_template('confs/details.html')
+    conf = get_object_or_404(Conf, pk=conf_id)
+    context = RequestContext(request, {'conf': conf})
+    return HttpResponse(template.render(context))
