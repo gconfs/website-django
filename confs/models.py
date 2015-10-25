e184b0004e3f63e82434639367e53186ca8e764b
Guillaume Doré <dore_g@epita.fr>
2015-10-25 12:08:51 +0100

    add embedded youtube videos

--- confs/models.py
+++ confs/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils import timezone
+
 from team.models import Member as Speaker
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -27,6 +31,16 @@ class VideoInfo(models.Model):
     conf = models.ForeignKey(Conf)
     videoinfo_url = models.URLField('URL')
     videoinfo_title = models.CharField('Titre', max_length=200)
-
+    youtube_id = models.CharField(max_length=200, editable=False)
+    class Meta:
+        ordering = ['id']
     def __str__(self):
         return self.videoinfo_url
+
+@receiver(pre_save, sender=VideoInfo)
+def get_youtube_id(sender, **kwargs):
+    instance = kwargs['instance']
+    url = instance.videoinfo_url.split('/')[3].split('=')
+    url = url[len(url) - 1]
+    instance.youtube_id = url.split('?')[0]
+    print(instance.youtube_id)
