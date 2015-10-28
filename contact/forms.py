9f085c4ef4404a1523280dd2a62b275cf9918ebb
Guillaume Doré <dore_g@epita.fr>
2015-10-28 18:42:36 +0100

    beautiful contact form

--- contact/forms.py
+++ contact/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from captcha.fields import ReCaptchaField
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
+    subject = forms.CharField(widget=forms.TextInput(attrs={'class' :
+        'form-control'}), max_length=100)
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    sender = forms.EmailField(widget=forms.TextInput(attrs={'class' :
+        'form-control'}), max_length=100)
     cc_myself = forms.BooleanField(required=False)
     captcha = ReCaptchaField()
