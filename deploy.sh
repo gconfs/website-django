bb575efe4912d3bb9d5e391e76eda6b984d3d954
Guillaume Doré <dore_g@epita.fr>
2015-12-23 13:41:08 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 cd $( dirname $0 )
 pip install -r requirements.txt
 
+cp ./gconfs_website/settings.py ./gconfs_website/prod.py
+sed -i "s/DEBUG = True/DEBUG = False/" ./gconfs_website/prod.py
+export DJANGO_SETTINGS_MODULE=gconfs_website.settings.prod
+
 [ $# -lt 2 ] && exit 0
 
 db=$1
@@ -11,7 +15,3 @@ user=$2
 
 sed -i "s/DB/$db/" ./psql_dump.sh
 sed -i "s/USER/$user/" ./psql_dump.sh
-
-cp ./gconfs_website/settings.py ./gconfs_website/prod.py
-sed -i "s/DEBUG = True/DEBUG = False/" ./gconfs_website/prod.py
-export DJANGO_SETTINGS_MODULE=gconfs_website.settings.prod
